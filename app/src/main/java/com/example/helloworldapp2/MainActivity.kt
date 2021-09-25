20a80fb3fe3ea5a0513c2bda8b4e9cce002d9f32
Zahra Yousef <45607455+zoozi515@users.noreply.github.com>
2021-09-25 19:07:08 +0300

    Hello World App (UI Design) - CVersion #1

---
+++ app/src/main/java/com/example/helloworldapp2/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.helloworldapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    //Cardview cardHome;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
